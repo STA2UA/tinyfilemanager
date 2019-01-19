f056aa487d35c18ba66dde38980f6723d5c63b7a
STA2UA <32482440+STA2UA@users.noreply.github.com>
2019-01-19 13:00:38 +0100

    Update tinyfilemanager.php

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -17,10 +17,9 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => '$2y$10$7/B4PCng1sZHcpSQWvB1g.KeYtJOb3AtYJqhxsYVUbtFio6E2RSBO', //admin@123
+    'user' => '$2y$10$9RrBpqDueSYANJxvjQ2pZuUfy4TxUNqjP//PH5dQL.m107aJn/Gg.' //12345
 );
-
 // Readonly users (username array)
 $readonly_users = array(
     'user'
